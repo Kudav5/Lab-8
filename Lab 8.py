c9fefbaf99ea6271c17f21bd10c146cfee86e243
Kudav5 <kdvalerian@gmail.com>
2022-12-12 11:31:03 +0700

    many bug

--- Lab 8.py
+++ Lab 8.py
@@ -1,40 +1,44 @@
-
-
 class Mahasiswa:
     def __init__(self, nama, nilai):
         self.nama = nama
         self.nilai = nilai
 
-    def tampilkan(self):        # untuk menampilkan data
+    @staticmethod
+    def tampilkan(nama, nilai):        # untuk menampilkan data
         tampil = {
-            self.nama:self.nilai
+            nama:nilai
         }
         print(tampil)
 
-class data(Mahasiswa):
-    def tambah(Mahasiswa):     # untuk menambah data
-        a = data[nama] = nilai
+    @staticmethod
+    def tambah(cls):     # untuk menambah data
+        # a = data[nama] = nilai
+
+    @staticmethod
     def hapus(nama):                        # untuk menghapus data berdasarkan nama
-        print('menghapus data', nama)
-        if nama in data.keys():
-            del data[nama]
-        return nama, 'sudah dihapus'
+        # print('menghapus data', nama)
+        # if nama in data.keys():
+        #     del data[nama]
+        # return nama, 'sudah dihapus'
+        return
+
+    @staticmethod
     def ubah(nama):                         # untuk mengubah data berdasarkan nama
-        print('mengubah data ', nama)
-        if data.keys():
-            del data[nama]
-            nama = input("Nama baru: ")
-            nilai = input('Nilai baru: ')
-        data[nama] = nilai
+        # print('mengubah data ', nama)
+        # if data.keys():
+        #     del data[nama]
+        #     nama = input("Nama baru: ")
+        #     nilai = input('Nilai baru: ')
+        # data[nama] = nilai
 
 
-mic = input('Tambah/Tampilkan/Hapus/Ubah : ')
-if mic=='Tambah':
-    Mahasiswa()
+mic = input('Tambah/Tampilkan/Hapus/Ubah : ')       # Expected indented block
+# if mic=='Tambah':
+#     Mahasiswa()
 
 
 nama = input('Nama Anda: ')
 nilai = str(input('Nilai Anda: '))
 
-a = Mahasiswa(nama,nilai)
-a.tampilkan()
+# a = Mahasiswa(nama,nilai)
+# a.tampilkan()
